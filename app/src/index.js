483239bd9e552e292324857dd04621e1a4a34149
amitrathik <amit.rathi.k@gmail.com>
2017-09-25 01:18:26 -0400

    cleanup

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SearchForm from './components/SearchForm';
+import SearchResultsList from './components/SearchResultsList';
+import LocationDetails from './components/LocationDetails';
 
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      locations: [],
+      selectedLocation: null,
+      conditions: null
+    };
+  }
+
+  handleLocationSearch(term){
+    return fetch(`http://localhost:3000/search/?term=${term}`)
+      .then((res) => res.json())
+      .then((res) => this.setState({locations : res['RESULTS']}))
+  }
+
+  handleLocationSelect(location){
+    return fetch(`http://localhost:3000/conditions/?lat=${location.lat}&lon=${location.lon}`)
+      .then((res) => res.json())
+      .then((res) => this.setState({conditions : res['current_observation']}))
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <SearchForm
+          onSearchTermChange={term=> this.handleLocationSearch(term)}
+        />
+        <SearchResultsList
+          locations={this.state.locations}
+          onLocationSelect={(selectedLocation) => this.setState({
+            selectedLocation : selectedLocation,
+            conditions : this.handleLocationSelect(selectedLocation)
+          })}
+        />
+        <LocationDetails
+          location={this.state.selectedLocation}
+          conditions={this.state.conditions}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
