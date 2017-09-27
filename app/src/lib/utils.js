a8285d4312e09793938acc96e7f8f542bdf49058
amitrathik <amit.rathi.k@gmail.com>
2017-09-26 23:20:02 -0400

    updates

--- app/src/lib/utils.js
+++ app/src/lib/utils.js
@@ -1,9 +1,10 @@
+// fetch location suggestions and return results
 export const loadLocations = (term) => {
   return fetch(`http://localhost:3000/search/?term=${term}`)
     .then(res => res.json())
     .then(res => res['RESULTS'])
 }
-
+// fetch conditions and return current observation
 export const loadConditions = (location) => {
   return fetch(`http://localhost:3000/conditions/?lat=${location.lat}&lon=${location.lon}`)
     .then(res => res.json())
